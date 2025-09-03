f15ef339a814b135f23a34b9ca5acf3b1728e84e
Idris jose <idrisjose11@gmail.com>
2025-09-03 18:29:06 +0100

    first commit

---
+++ src/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+export default function LandingPage(){
+    return(
+        <div>
+            <div>
+
+            </div>
+
+            <div>
+                <h1>One link to connect<br/>everything</h1>
+            </div>
+        </div>
+    )
+}
